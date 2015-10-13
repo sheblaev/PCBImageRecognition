f9123c5e0e98a007232ba56419ba031e4ab2c24f
sheblaev <sheblaev@gmail.com>
2015-10-13 22:01:49 +0300

    modified:   contour.py

--- contour.py
+++ contour.py
@@ -1,33 +1,16 @@
 # USAGE
-# python sorting_contours.py --image images/image_01.png --method "top-to-bottom"
-
+# python sorting_contours.py 
 # import the necessary packages
 import numpy as np
-import argparse
 import cv2
+from matplotlib import pyplot as plt
 
-def sort_contours(cnts, method="left-to-right"):
-    # initialize the reverse flag and sort index
-    reverse = False
-    i = 0
+        
+def imshow(name, image):
+    plt.imshow(image)
+    plt.title(name)    
+    plt.show()
 
-    # handle if we need to sort in reverse
-    if method == "right-to-left" or method == "bottom-to-top":
-        reverse = True
-
-    # handle if we are sorting against the y-coordinate rather than
-    # the x-coordinate of the bounding box
-    if method == "top-to-bottom" or method == "bottom-to-top":
-        i = 1
-
-    # construct the list of bounding boxes and sort them from top to
-    # bottom
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-        key=lambda b:b[1][i], reverse=reverse))
-
-    # return the list of sorted contours and bounding boxes
-    return (cnts, boundingBoxes)
 
 def draw_contour(image, c, i):
     # compute the center of the contour area and draw a circle
@@ -42,22 +25,21 @@ def draw_contour(image, c, i):
     # draw the countour number on the image
     cv2.putText(image, "#{}".format(i + 1), (cX - 20, cY), cv2.FONT_HERSHEY_SIMPLEX,
         1.0, (255, 255, 255), 2)
-    cv2.drawContours(image, c, -1, (0, 255,  255))
 
+    cv2.drawContours(image, c, -1, (0, 255,  255))
+    rect = cv2.minAreaRect(c)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(image,[box],0,(0,255,0 ),2)
     # return the image with the contour number drawn on it
     return image
 
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the input image")
-ap.add_argument("-m", "--method", required=True, help="Sorting method")
-args = vars(ap.parse_args())
-
 # load the image and initialize the accumulated edge image
-image = cv2.imread("photo2.jpg")
+image = cv2.imread("photo1.jpg")
 gray = image
 #cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("image", image)
+imshow("image", image)
 
 # in both the x and y direction
 gray = image
@@ -70,8 +52,8 @@ gray = image
 
 # blur and threshold the image
 blurred = cv2.blur(gray, (5, 5))
-cv2.imshow("gray", gray)
-cv2.imshow("blurred", blurred)
+imshow("gray", gray)
+imshow("blurred", blurred)
 (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 closed= blurred
 # construct a closing kernel and apply it to the thresholded image
@@ -89,18 +71,18 @@ accumEdged = np.zeros(image.shape[:2], dtype="uint8")
 for chan in cv2.split(image):
     # blur the channel, extract edges from it, and accumulate the set
     # of edges for the image
-    cv2.imshow("chan", chan)
     chan = cv2.medianBlur(chan, 11)
+    imshow("chan", chan)
 
 edged = cv2.Canny(closed, 150, 250)
 accumEdged = cv2.bitwise_or(accumEdged, edged)
 
 # show the accumulated edge map
-cv2.imshow("Edge Map", accumEdged)
+imshow("Edge Map", accumEdged)
 
 # find contours in the accumulated image, keeping only the largest
 # ones
-(_, cnts, _) = cv2.findContours(accumEdged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+(_, cnts, _) = cv2.findContours(accumEdged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5:]
 orig = image.copy()
 
@@ -109,7 +91,7 @@ for (i, c) in enumerate(cnts):
     orig = draw_contour(orig, c, i)
 
 # show the original, unsorted contour image
-cv2.imshow("Unsorted", orig)
+imshow("Unsorted", orig)
 
 # sort the contours according to the provided method
 #(cnts, boundingBoxes) = sort_contours(cnts, method=args["method"])
@@ -119,5 +101,5 @@ for (i, c) in enumerate(cnts):
     draw_contour(image, c, i)
 
 # show the output image
-cv2.imshow("Sorted", image)
+imshow("Sorted", image)
 cv2.waitKey(0)
